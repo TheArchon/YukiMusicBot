a5abd734be08150319f7314423e86ae9486ce0a2
𝐀ʀᴄʜᴏɴ [🇮🇳] <teamoctavex@gmail.com>
2026-09-23 18:52:34 +0530

    Refactor settings.py for better organization

--- YukiMusic/plugins/bot/settings.py
+++ YukiMusic/plugins/bot/settings.py
@@ -189,12 +189,12 @@ async def send_repo(_, message: Message):
         caption=(
             "<b>✨ ʜᴇʏ ᴅᴇᴀʀ, ʜᴇʀᴇ ɪꜱ ᴛʜᴇ ᴏꜰꜰɪᴄɪᴀʟ ʀᴇᴘᴏꜱɪᴛᴏʀʏ ᴏꜰ ᴛʜɪꜱ ʙᴏᴛ ✨</b>\n\n"
             "🔗 ᴅᴏɴ'ᴛ ꜰᴏʀɢᴇᴛ ᴛᴏ ɢɪᴠᴇ ᴀ ꜱᴛᴀʀ 🌟 ᴀɴᴅ ꜰᴏʟʟᴏᴡ!\n\n"
-            "🧡 ᴄʀᴇᴅɪᴛꜱ : <a href='https://t.me/ShrutiBots'>@ShrutiBots</a>"
+            "🧡 ᴄʀᴇᴅɪᴛꜱ : <a href='https://t.me/ArchonNetwork'>@ArchonNetwork</a>"
         ),
         reply_markup=InlineKeyboardMarkup(
             [
-                [InlineKeyboardButton("🎵 Music + Management Bot Repo", url="https://github.com/NoxxOP/ShrutiMusic")],
-                [InlineKeyboardButton("🎧 Only Music Bot Repo", url="https://github.com/NoxxOP/YukiMusic")]
+                [InlineKeyboardButton("🎵 Music + Management Bot Repo", url="https://github.com/TheArchon/YukiMusic")],
+                [InlineKeyboardButton("🎧 Only Music Bot Repo", url="https://github.com/TheArchon/YukiMusic")]
             ]
         ),
         supports_streaming=True,

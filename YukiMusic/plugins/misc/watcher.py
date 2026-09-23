ae0320ec9aed8e2ad158c9708df0039edc725d9d
𝐀ʀᴄʜᴏɴ [🇮🇳] <teamoctavex@gmail.com>
2026-09-23 12:34:56 +0530

    Add files via upload

--- YukiMusic/plugins/misc/watcher.py
+++ YukiMusic/plugins/misc/watcher.py
@@ -1,1 +1,14 @@
+from pyrogram import filters
+from pyrogram.types import Message
 
+from YukiMusic import yuki
+from YukiMusic.core.call import Shruti
+
+welcome = 20
+close = 30
+
+
+@yuki.on_message(filters.video_chat_started, group=welcome)
+@yuki.on_message(filters.video_chat_ended, group=close)
+async def welcome(_, message: Message):
+    await Shruti.stop_stream_force(message.chat.id)
